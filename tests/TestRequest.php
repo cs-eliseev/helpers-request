afd056732267dae2632d5a56e02cf4f8c7b893fa
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-25 08:23:46 +0300

    add unit test method GET data by key name

--- tests/TestRequest.php
+++ tests/TestRequest.php
@@ -26,6 +26,25 @@ class TestRequest extends TestCase
     }
 
     /**
+     * @param string $key
+     * @param null|string $default
+     * @param null|string $value
+     * @param bool $set
+     * @param $expected
+     *
+     * @dataProvider providerRequest
+     */
+    public function testGet(string $key, ?string $default, ?string $value, bool $set, $expected): void
+    {
+        if ($set) {
+            $_GET[$key] = $value;
+        } else {
+            unset($_GET[$key]);
+        }
+        $this->assertEquals($expected, Request::get($key, $default));
+    }
+
+    /**
      * @return array
      */
     public function providerRequest(): array
