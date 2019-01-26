49e3252c80b531835a6a2e596ad03354fa16f082
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-26 07:50:05 +0300

    add method get Request data by key name

--- src/Request.php
+++ src/Request.php
@@ -34,4 +34,16 @@ class Request
     {
         return $_GET[$key] ?? $default;
     }
+
+    /**
+     * Get Request date by key name
+     *
+     * @param $key
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function request($key, $default = null)
+    {
+        return $_REQUEST[$key] ?? $default;
+    }
 }
